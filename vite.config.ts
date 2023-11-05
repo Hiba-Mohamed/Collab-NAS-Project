a1813360e70e8dabbdffd36c487eb3d9351484ea
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-11-05 20:04:30 -0330

    title adjusted

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Reactjs-Nurses-Assignment-Sheet/',
+  base: '/Collab-NAS-Project/',
   optimizeDeps: {
     include: ['*.ts', '*.d.ts', '*.tsx', '*.js', '*.jsx'],
   },
