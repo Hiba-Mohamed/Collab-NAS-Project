de41eed1c62ef659e474df8b451e75e7a8db5e06
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2024-02-02 22:08:31 -0330

    styling intro page

--- src/pages/introPage.tsx
+++ src/pages/introPage.tsx
@@ -4,7 +4,6 @@ export function LandingPage() {
   return (
     <div className="min-h-screen items-center bg-sky-50 font-OpenSans flex flex-col max-w-xl sm:max-w-full px-2">
       <div className="flex flex-row bg-[url('/images/banner.png')] w-full items-center justify-center bg-cover">
-
         <div className="max-w-3xl flex flex-col w-full items-center pt-12">
           <div className="flex sm:pt-12 flex-col  lg:text-5xl items-center py-4 text-blue text-2xl sm:text-4xl text-center font-bold">
             <p>Welcome to the Electronic NAS </p>
@@ -26,138 +25,123 @@ export function LandingPage() {
             Get Started!
           </Link>
         </div>
-
       </div>
       <div className="flex flex-row items-center sm:mt-28 mx-4 p-4 text-start text-lg">
         <p className="font-bold text-blue text-xl sm:text-2xl lg:text-4xl">
           A Quick Guide to Get Started Below:
         </p>
       </div>
-      <div className="flex flex-col justify-evenly p-2 items-center">
-        <div className=" flex flex-col w-full justify-evenly lg:flex-row">
-          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start max-w-xl m-2 py-4 w-80 lg:w-500">
-            <div className="flex lg:flex-row-reverse flex-col gap-2">
-              <img
-                src="images/features1.png"
-                alt=""
-                className="rounded-xl lg:w-64"
-              />
-              <div className="lg:w-56 lg:text-lg">
-                <div className="pb-4">
-                  <p className="font-extrabold">Step 1: </p>
-                  <p className="font-extrabold">Create Hospital Profile</p>
-                </div>
-                <p className="sm:text-sm">
+      <div className="flex flex-col justify-evenly items-center">
+        <div className=" flex flex-col justify-evenly lg:flex-row">
+          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start m-2 py-4">
+            <div className="flex flex-col gap-2">
+              <div className="pb-4 w-300 sm:w-400">
+                <p className="font-extrabold">
+                  Step 1: Create Hospital Profile
+                </p>
+                <p className="">
                   Click on "Get Started" button above to create a Hospital
                   profile.
                 </p>
               </div>
+              <img
+                src="images/features1.png"
+                alt=""
+                className="rounded-xl w-300 sm:w-400"
+              />
             </div>
           </div>
-          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start max-w-xl m-2 py-4 w-80 lg:w-500">
-            <div className="flex lg:flex-row-reverse flex-col gap-2">
+          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start m-2 py-4">
+            <div className="flex flex-col gap-2">
+              <div className="pb-4 w-300 sm:w-400">
+                <p className="font-extrabold">
+                  Step 2: Create New Unit profile
+                </p>
+                <p className="">Type your new hospital units's name.</p>
+              </div>
               <img
                 src="images/features6.png"
                 alt=""
-                className="rounded-xl lg:w-56"
+                className="rounded-xl w-300 sm:w-400"
               />
-              <div className="lg:w-56 lg:text-lg">
-                <div className="pb-4">
-                  <p className="font-extrabold">Step 2: </p>
-                  <p className="font-extrabold">Create New Unit profile</p>
-                </div>
-                <p className="sm:text-sm">
-                  Type your new hospital units's name.
-                </p>
-              </div>
             </div>
           </div>
         </div>
         <div className=" flex flex-col lg:flex-row ">
-          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start max-w-xl m-2 py-4 w-80 lg:w-500">
-            <div className="flex lg:flex-row-reverse flex-col gap-2">
-              <img
-                src="images/features5.png"
-                alt=""
-                className="rounded-xl lg:w-56"
-              />
-              <div className="lg:w-56 lg:text-lg">
-                <div className="pb-4">
-                  <p className="font-extrabold">Step 3: </p>
-                  <p className="font-extrabold">Create New Shifts</p>
-                </div>
-                <p className="sm:text-sm">
+          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start m-2 py-4">
+            <div className="flex flex-col gap-2">
+              <div className="pb-4 w-300 sm:w-400">
+                <p className="font-extrabold">Step 3: Create New Shifts</p>
+                <p className="">
                   Click on the icon on the left to start creating shifts for the
                   unit.
                 </p>
-                <p className="pt-4 sm:text-sm">
-                  Specify the date and type of the shift.
-                </p>{" "}
+                <p>Specify the date and type of the shift.</p>
               </div>
+              <img
+                src="images/features5.png"
+                alt=""
+                className="rounded-xl w-300 sm:w-400"
+              />
             </div>
           </div>
-          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start max-w-xl m-2 py-4 w-80 lg:w-500">
-            <div className="flex lg:flex-row-reverse flex-col gap-2">
+          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start m-2 py-4">
+            <div className="flex flex-col gap-2">
+              <div className="pb-4 w-300 sm:w-400">
+                <p className="font-extrabold">
+                  Step 4: Manage Shifts Assignments
+                </p>
+                <p className="">
+                  Edit shift and assignment information as necessary.
+                </p>
+                <p>Specify the date and type of the shift.</p>
+              </div>
               <img
                 src="images/features4.png"
                 alt=""
-                className="rounded-xl lg:w-64"
+                className="rounded-xl w-300 sm:w-400"
               />
-              <div className="lg:w-56 lg:text-lg">
-                <div className="pb-4">
-                  <p className="font-extrabold">Step 4: </p>
-                  <p className="font-extrabold">Manage Shifts Assignments</p>
-                </div>
-                <p className="sm:text-sm">
-                  Edit shift and assignment information as necessary.
-                </p>
-              </div>
             </div>
           </div>
         </div>
         <div className=" flex flex-col lg:flex-row ">
-          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start max-w-xl m-2 py-4 w-80 lg:w-500">
-            <div className="flex lg:flex-row-reverse flex-col gap-2">
-              <img
-                src="images/features5.png"
-                alt=""
-                className="rounded-xl lg:w-56"
-              />
-              <div className="lg:w-56 lg:text-lg">
-                <div className="pb-4">
-                  <p className="font-extrabold">Step 5: </p>
-                  <p className="font-extrabold">Review or Search for Shifts</p>
-                </div>
-                <p className="sm:text-sm">
-                  {" "}
+          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start m-2 py-4">
+            <div className="flex flex-col gap-2">
+              <div className="pb-4 w-300 sm:w-400">
+                <p className="font-extrabold">
+                  Step 5: Review or Search for Shifts
+                </p>
+                <p className="">
                   Click on the icon on the left to access your shifts record.
                 </p>
-                <p className="sm:text-sm pt-2">
+                <p>
                   Shifts are autosaved and organized from newest to oldest. Use
                   the search bar to quickly find a specific shift.
                 </p>
               </div>
+              <img
+                src="images/features5.png"
+                alt=""
+                className="rounded-xl w-300 sm:w-400"
+              />
             </div>
           </div>
-          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start max-w-xl m-2 py-4 w-80 lg:w-500">
-            <div className="flex lg:flex-row-reverse flex-col gap-2">
+          <div className="flex flex-col rounded-lg bg-white flex flex-row items-start px-6 shadow-lg text-start m-2 py-4">
+            <div className="flex flex-col gap-2">
+              <div className="pb-4 w-300 sm:w-400">
+                <p className="font-extrabold">
+                  Step 6: Review Hospital Profile
+                </p>
+                <p className="">
+                  Click on "Hospital Units" from the navigation menu.
+                </p>
+                <p>Review and edit your Hospital profile as necassary.</p>
+              </div>
               <img
                 src="images/features3.png"
                 alt=""
-                className="rounded-xl lg:w-56"
+                className="rounded-xl w-300 sm:w-400"
               />
-              <div className="lg:w-56 lg:text-lg">
-                <div className="pb-4">
-                  <p className="font-extrabold">Step 5: </p>
-                  <p className="font-extrabold">Review Hospital Profile</p>
-                </div>
-                <p className="sm:text-sm">
-                  Click on "Hospital Units" from the navigation menu.
-                </p>
-                <p className="sm:text-sm pt-2">
-                  Review and edit your Hospital profile as necassary.
-                </p>
-              </div>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      width: { 500: "500px", 600: "600px", 700: "700px", 800: "800px" },
+      width: { 200: "200px", 250: "250px", 300: "300px", 350: "350px", 400: "500px", 500: "500px", 600: "600px", 700: "700px", 800: "800px" },
       height:{1:"1px", 2:"2px", 1.5:"1.5px"},
       colors: {
         peach: "#EF798A",
